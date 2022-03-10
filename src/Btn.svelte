dc952cb38ab7036c478b9d34f9d5e611fc9fe5db
Thiago Lagden <lagden@gmail.com>
2022-03-10 00:58:50 -0300

    fix vars

--- src/Btn.svelte
+++ src/Btn.svelte
@@ -2,130 +2,82 @@
 	let className = ''
 	export {className as class}
 
-	export let useDefaultFX = false
-	export let useOutline = false
-
 	$$restProps.type = $$restProps?.type ?? 'button'
 </script>
 
 <button
 	class="_tadashi_svelte_btn {className}"
-	class:_tadashi_svelte_btn___default_fx={useDefaultFX}
-	class:_tadashi_svelte_btn___outline={useOutline}
 	on:click
 	{...$$restProps}
 ><slot /></button>
 
 <style>
 	:root {
-		--tadashi_svelte_btn_align_items: center;
-		--tadashi_svelte_btn_background_color: hsl(210deg 50% 50%);
-		--tadashi_svelte_btn_border: none;
-		--tadashi_svelte_btn_border_radius: 0.15em;
-		--tadashi_svelte_btn_box_shadow: none;
-		--tadashi_svelte_btn_color: hsl(0deg 0% 100%);
-		--tadashi_svelte_btn_cursor: pointer;
-		--tadashi_svelte_btn_filter: none;
-		--tadashi_svelte_btn_font_size: 1em;
-		--tadashi_svelte_btn_font_weight: 300;
-		--tadashi_svelte_btn_grid_gap: 5px;
-		--tadashi_svelte_btn_height: 36px;
-		--tadashi_svelte_btn_outline: 0;
-		--tadashi_svelte_btn_opacity: 1;
-		--tadashi_svelte_btn_padding: 0 1em;
-		--tadashi_svelte_btn_transform:  none;
-		--tadashi_svelte_btn_transition_property: background-color, border-color, box-shadow, color, filter, transform;
-		--tadashi_svelte_btn_transition_duration: 0.5s;
-
-		--tadashi_svelte_btn___disabled_opacity: 0.5;
-
-		--tadashi_svelte_btn__outline_background_color: hsl(0deg 0% 100%);
-		--tadashi_svelte_btn__outline_color: hsl(240deg 9% 44%);
-		--tadashi_svelte_btn__outline_border: 1px solid hsl(240deg 17% 88%);
-		--tadashi_svelte_btn__outline_box_shadow: 0px 3px 0px 0px hsl(240deg 17% 88%);
-		--tadashi_svelte_btn__outline___not_disabled___focus_background_color: hsl(240deg 17% 88% / 30%);
-		--tadashi_svelte_btn__outline___not_disabled___hover_background_color: hsl(240deg 17% 88% / 30%);
-		--tadashi_svelte_btn__outline___not_disabled___active_box_shadow: 0px 0px 0px 0px hsl(240deg 17% 88%);
-		--tadashi_svelte_btn__outline___not_disabled___active_transform: translateY(3px);
-
+		--tadashi_svelte_btn_common_primary: hsl(0deg 0% 100%);
+		--tadashi_svelte_btn_common_secondary: hsl(240deg 9% 44%);
+		--tadashi_svelte_btn_common_tertiary: hsl(240deg 17% 88%);
+		--tadashi_svelte_btn_common_quaternary: hsl(240deg 17% 88% / 30%);
 	}
 
 	._tadashi_svelte_btn {
-		align-items: var(--tadashi_svelte_btn_align_items);
-		background-color: var(--tadashi_svelte_btn_background_color);
-		border: var(--tadashi_svelte_btn_border);
-		border-radius: var(--tadashi_svelte_btn_border_radius);
-		box-shadow: var(--tadashi_svelte_btn_box_shadow);
+		align-items: var(--tadashi_svelte_btn_align_items, center);
+		background-color: var(--tadashi_svelte_btn_background_color, var(--tadashi_svelte_btn_common_primary));
+		border: var(--tadashi_svelte_btn_border, 1px solid var(--tadashi_svelte_btn_common_tertiary));
+		border-radius: var(--tadashi_svelte_btn_border_radius, 0.15em);
+		box-shadow: var(--tadashi_svelte_btn_box_shadow, 0px 3px 0px 0px var(--tadashi_svelte_btn_common_tertiary));
 		box-sizing: border-box;
-		color: var(--tadashi_svelte_btn_color);
-		cursor: var(--tadashi_svelte_btn_cursor);
-		filter: var(--tadashi_svelte_btn_filter);
-		font-size: var(--tadashi_svelte_btn_font_size);
-		font-weight: var(--tadashi_svelte_btn_font_weight);
-		height: var(--tadashi_svelte_btn_height);
-		opacity: var(--tadashi_svelte_btn_opacity);
-		outline: var(--tadashi_svelte_btn_outline);
+		color: var(--tadashi_svelte_btn_color, var(--tadashi_svelte_btn_common_secondary));
+		cursor: var(--tadashi_svelte_btn_cursor, pointer);
+		filter: var(--tadashi_svelte_btn_filter, none);
+		font-size: var(--tadashi_svelte_btn_font_size, 1em);
+		font-weight: var(--tadashi_svelte_btn_font_weight, 300);
+		height: var(--tadashi_svelte_btn_height, 36px);
+		opacity: var(--tadashi_svelte_btn_opacity, 1);
+		outline: var(--tadashi_svelte_btn_outline, 0);
 		overflow: hidden;
-		padding: var(--tadashi_svelte_btn_padding);
+		padding: var(--tadashi_svelte_btn_padding, 0 1em);
 		position: relative;
-		transform: var(--tadashi_svelte_btn_transform);
+		transform: var(--tadashi_svelte_btn_transform, none);
 		user-select: none;
 
-		transition-duration: var(--tadashi_svelte_btn_transition_duration);
-		transition-property: var(--tadashi_svelte_btn_transition_property);
+		transition-duration: var(--tadashi_svelte_btn_transition_duration, 0.5s);
+		transition-property: var(--tadashi_svelte_btn_transition_property, background-color, border-color, box-shadow, color, filter, transform);
 
-		display: inline-grid;
-		grid-auto-flow: column;
-		grid-template-columns: auto;
-		grid-gap: var(--tadashi_svelte_btn_grid_gap);
+		display: var(--tadashi_svelte_btn_display, inline-grid);
+		grid-auto-flow: var(--tadashi_svelte_btn_grid_auto_flow, column);
+		grid-template-columns: var(--tadashi_svelte_btn_grid_template_columns, auto);
+		grid-gap: var(--tadashi_svelte_btn_grid_gap, 5px);
 	}
 
 	._tadashi_svelte_btn:disabled {
 		cursor: not-allowed;
-		opacity: var(--tadashi_svelte_btn___disabled_opacity, var(--tadashi_svelte_btn_opacity));
+		opacity: var(--tadashi_svelte_btn___disabled_opacity, 0.5);
 	}
 
 	._tadashi_svelte_btn:not(:disabled):focus {
-		background-color: var(--tadashi_svelte_btn___not_disabled___focus_background_color, var(--tadashi_svelte_btn_background_color));
-		border: var(--tadashi_svelte_btn___not_disabled___focus_border, var(--tadashi_svelte_btn_border));
-		box-shadow: var(--tadashi_svelte_btn___not_disabled___focus_box_shadow, var(--tadashi_svelte_btn_box_shadow));
-		color: var(--tadashi_svelte_btn___not_disabled___focus_color, var(--tadashi_svelte_btn_color));
-		filter: var(--tadashi_svelte_btn___not_disabled___focus_filter, var(--tadashi_svelte_btn_filter));
-		transform: var(--tadashi_svelte_btn___not_disabled___focus_transform, var(--tadashi_svelte_btn_transform));
+		background-color: var(--tadashi_svelte_btn___not_disabled___focus_background_color, var(--tadashi_svelte_btn_common_quaternary));
+		border: var(--tadashi_svelte_btn___not_disabled___focus_border, var(--tadashi_svelte_btn_border, 1px solid var(--tadashi_svelte_btn_common_tertiary)));
+		box-shadow: var(--tadashi_svelte_btn___not_disabled___focus_box_shadow, var(--tadashi_svelte_btn_box_shadow, 0px 3px 0px 0px var(--tadashi_svelte_btn_common_tertiary)));
+		color: var(--tadashi_svelte_btn___not_disabled___focus_color, var(--tadashi_svelte_btn_color, var(--tadashi_svelte_btn_common_secondary)));
+		filter: var(--tadashi_svelte_btn___not_disabled___focus_filter, var(--tadashi_svelte_btn_filter, none));
+		transform: var(--tadashi_svelte_btn___not_disabled___focus_transform, var(--tadashi_svelte_btn_transform, none));
 	}
 
 	._tadashi_svelte_btn:not(:disabled):hover {
-		background-color: var(--tadashi_svelte_btn___not_disabled___hover_background_color, var(--tadashi_svelte_btn_background_color));
-		border: var(--tadashi_svelte_btn___not_disabled___hover_border, var(--tadashi_svelte_btn_border));
-		box-shadow: var(--tadashi_svelte_btn___not_disabled___hover_box_shadow, var(--tadashi_svelte_btn_box_shadow));
-		color: var(--tadashi_svelte_btn___not_disabled___hover_color, var(--tadashi_svelte_btn_color));
-		filter: var(--tadashi_svelte_btn___not_disabled___hover_filter, var(--tadashi_svelte_btn_filter));
-		transform: var(--tadashi_svelte_btn___not_disabled___hover_transform, var(--tadashi_svelte_btn_transform));
+		background-color: var(--tadashi_svelte_btn___not_disabled___hover_background_color, var(--tadashi_svelte_btn_common_quaternary));
+		border: var(--tadashi_svelte_btn___not_disabled___hover_border, var(--tadashi_svelte_btn_border, 1px solid var(--tadashi_svelte_btn_common_tertiary)));
+		box-shadow: var(--tadashi_svelte_btn___not_disabled___hover_box_shadow, var(--tadashi_svelte_btn_box_shadow, 0px 3px 0px 0px var(--tadashi_svelte_btn_common_tertiary)));
+		color: var(--tadashi_svelte_btn___not_disabled___hover_color, var(--tadashi_svelte_btn_color, var(--tadashi_svelte_btn_common_secondary)));
+		filter: var(--tadashi_svelte_btn___not_disabled___hover_filter, var(--tadashi_svelte_btn_filter, none));
+		transform: var(--tadashi_svelte_btn___not_disabled___hover_transform, var(--tadashi_svelte_btn_transform, none));
 	}
 
 	._tadashi_svelte_btn:not(:disabled):active {
-		background-color: var(--tadashi_svelte_btn___not_disabled___active_background_color, var(--tadashi_svelte_btn_background_color));
-		border: var(--tadashi_svelte_btn___not_disabled___active_border, var(--tadashi_svelte_btn_border));
-		box-shadow: var(--tadashi_svelte_btn___not_disabled___active_box_shadow, var(--tadashi_svelte_btn_box_shadow));
-		color: var(--tadashi_svelte_btn___not_disabled___active_color, var(--tadashi_svelte_btn_color));
-		filter: var(--tadashi_svelte_btn___not_disabled___active_filter, var(--tadashi_svelte_btn_filter));
-		transform: var(--tadashi_svelte_btn___not_disabled___active_transform, var(--tadashi_svelte_btn_transform));
-	}
-
-	._tadashi_svelte_btn___default_fx {
-		--tadashi_svelte_btn___not_disabled___focus_filter: var(--tadashi_svelte_btn__fx___not_disabled___focus_filter, brightness(1.3));
-		--tadashi_svelte_btn___not_disabled___hover_filter: var(--tadashi_svelte_btn__fx___not_disabled___hover_filter, brightness(1.3));
-		--tadashi_svelte_btn___not_disabled___active_filter: var(--tadashi_svelte_btn__fx___not_disabled___active_filter, brightness(1.5));
-	}
-
-	._tadashi_svelte_btn___outline {
-		--tadashi_svelte_btn_background_color: var(--tadashi_svelte_btn__outline_background_color);
-		--tadashi_svelte_btn_color: var(--tadashi_svelte_btn__outline_color);
-		--tadashi_svelte_btn_border: var(--tadashi_svelte_btn__outline_border);
-		--tadashi_svelte_btn_box_shadow: var(--tadashi_svelte_btn__outline_box_shadow);
-		--tadashi_svelte_btn___not_disabled___focus_background_color: var(--tadashi_svelte_btn__outline___not_disabled___focus_background_color);
-		--tadashi_svelte_btn___not_disabled___hover_background_color: var(--tadashi_svelte_btn__outline___not_disabled___hover_background_color);
-		--tadashi_svelte_btn___not_disabled___active_box_shadow: var(--tadashi_svelte_btn__outline___not_disabled___active_box_shadow);
-		--tadashi_svelte_btn___not_disabled___active_transform: var(--tadashi_svelte_btn__outline___not_disabled___active_transform);
+		background-color: var(--tadashi_svelte_btn___not_disabled___active_background_color, var(--tadashi_svelte_btn_common_quaternary));
+		border: var(--tadashi_svelte_btn___not_disabled___active_border, var(--tadashi_svelte_btn_border, 1px solid var(--tadashi_svelte_btn_common_tertiary)));
+		box-shadow: var(--tadashi_svelte_btn___not_disabled___active_box_shadow, 0px 0px 0px 0px var(--tadashi_svelte_btn_common_tertiary));
+		color: var(--tadashi_svelte_btn___not_disabled___active_color, var(--tadashi_svelte_btn_color, var(--tadashi_svelte_btn_common_secondary)));
+		filter: var(--tadashi_svelte_btn___not_disabled___active_filter, var(--tadashi_svelte_btn_filter, none));
+		transform: var(--tadashi_svelte_btn___not_disabled___active_transform, translateY(3px));
 	}
 </style>
